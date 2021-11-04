ffa6be46663d3f931f10772ab7c291556bd7d67b
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-11-04 14:37:12 -0700

    delete routes and controllers to be refactored into GraphQL resolvers and typeDefs

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+const { ApolloServer } = require('apollo-server-express');
+
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +16,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
