1838b75be83552d57f4cd078498cbcc71558b2d1
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-11-04 15:02:48 -0700

    Set up Apollo server and type User

--- server/server.js
+++ server/server.js
@@ -3,11 +3,36 @@ const path = require('path');
 
 const { ApolloServer } = require('apollo-server-express');
 
+// import typeDefs & resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const startServer = async () => {
+  // create a new Apollo server and pass in schema data
+  const { authMiddleware } = require('./utils/auth');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+  // start the apollo server
+  await server.start();
+  
+  // integrate the Apollo server with the express application as middleware
+  server.applyMiddleware({ app });
+
+  // log where to go to test the GQL API
+  console.log(`User GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+};
+
+// initialize the Apollo server
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,6 +42,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
