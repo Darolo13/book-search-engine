d47a54b68a383e6fb05860e71619dad738c72f29
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-11-04 14:40:25 -0700

    Update authMiddleware function to work with the GraphQL API

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const { ApolloServer } = require('apollo-server-express');
 
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
